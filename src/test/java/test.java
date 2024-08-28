dda4c6f0bb8366d4a35c9b5d5f94cebfb870be84
Irina2294 <islivinscaia@gmail.com>
2024-08-27 21:08:49 -0500

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println(" hello world");
         System.out.println("this is the project version 1");
+        System.out.println("this is the project version 1");
     }
 }
 //done with version one
